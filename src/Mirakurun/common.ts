b201d572dc86dcff0fa499a52196cc7fa2b07f14
Oomugi413 <129721845+Oomugi413@users.noreply.github.com>
2025-03-30 18:50:22 +0900

    Update common.ts

--- src/Mirakurun/common.ts
+++ src/Mirakurun/common.ts
@@ -47,7 +47,7 @@ export interface StreamInfo {
 }
 
 export const channelTypes: apid.ChannelType[] = ["GR", "BS", "CS", "SKY", "BS4K"];
-
+export type ChannelType = keyof typeof ChannelTypes;
 export const deepClone = rfdc();
 
 export function updateObject<T, U>(target: T, input: U): boolean;
